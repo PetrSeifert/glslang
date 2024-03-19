cd12c6176b122c13e75f449ef20666651ac4553e
PetrSeifert <petr.seifert02@gmail.com>
2024-03-19 13:53:17 +0100

    Add premake

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "Glslang"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"glslang/**.h",
+		"glslang/**.cpp",
+		"SPIRV/**.cpp",
+		"SPIRV/**.h",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++20"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
